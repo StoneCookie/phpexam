5f5dd74cd8a5429bd0c73591bdd3005aba7c53b5
Perezhogin Fedor <pravsha63@gmai.com>
2020-06-25 13:00:13 +0300

    second commit

--- index.php
+++ index.php
@@ -3,12 +3,19 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/style.css">
 	<title>Document</title>
 </head>
 <body>
 	<!-- Шапка сайта -->
 	<header>
-
+		<h1 class="logo">Новая форма</h1>
+		<div class="wrap-nav">
+			<nav class="nav">
+				<a class="nav__link" href="">Вопрос</a>
+				<a class="nav__link" href="">Ответ</a>
+			</nav>
+		</div>
 	</header>
 
 	<!-- Контент сайта -->
