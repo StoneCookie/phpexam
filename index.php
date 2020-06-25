8e054d4e32374df68800e2543b3d3c102a632560
Perezhogin Fedor <pravsha63@gmai.com>
2020-06-25 10:01:55 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<!-- Шапка сайта -->
+	<header>
+
+	</header>
+
+	<!-- Контент сайта -->
+	<main>
+
+	</main>
+
+	<!-- Подвал сайта -->
+	<footer>
+
+	</footer>
+</body>
+</html>
